f7cc0b65cd8dc26ad4e43e0eec8c2f3f26ba75d6
brogatoga <36460435+brogatoga@users.noreply.github.com>
2024-10-24 11:27:12 -0400

    Create Meganbanner.py
    print name as banner

---
+++ Meganbanner.py
@@ -0,0 +1,21 @@
+'''
+
+ Name: Megan Broga
+ Date:Oct 24, 2024
+ Course Number: CSC 113
+ Course Name: Intro to Python Programming
+ Problem Number: Chapter 1 Assignment
+ Email: mfbroga2401@stcc.edu
+ Problem Description: Print name as banner of strings
+
+
+@author: meganbroga
+'''
+print("MM         MM  EEEEEEE  GGGGGGGG         A         NN       N")
+print("M  M     M  M  E        G               A A        N N      N")
+print("M   M   M   M  E        G              A   A       N  N     N")
+print("M     M     M  E        G             AAAAAAA      N   N    N")
+print("M           M  EEEEE    G   GGGG     A       A     N    N   N")
+print("M           M  E        G      G    A         A    N     N  N")
+print("M           M  E        G      G   A           A   N      N N")
+print("M           M  EEEEEEE  GGGGGGGG  A             A  N       N")
